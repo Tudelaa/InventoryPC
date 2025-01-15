d4227c7a93dc8882ac19a3a7ab2f754fa7494537
Antonio Tudela Botella <108870102+Tudelaa@users.noreply.github.com>
2025-01-15 17:31:31 +0100

    Update InventoryPC.ps1

--- InventoryPC.ps1
+++ InventoryPC.ps1
@@ -1,4 +1,4 @@
-﻿# inventoryPC 1.0
+# inventoryPC 1.10
 
 # this script generate a full inventory from your server with hostname, IP, Operating system, RAM, system model, SSID, Password 
 # and all the drives that you have including full size and free size.
@@ -8,51 +8,24 @@
 # get some extra values from get-computerinfo instead of systeminfo.
 # added microprocessor info
 # added kind license activation -OEM or KMS-
+# added S.M.A.R.T physical disk values
 
+
+# get different variables to make inventory with some cmd-lets commands
 
 $values = Get-ComputerInfo
 $net_values = Get-NetIPConfiguration
-
-#$hostname1 = systeminfo | Select-String "Host Name:"
-#$hostname2 = $hostname1.ToString().Split(":")[1].Trim()
-
 $hostname1 = $values.CsName 
-
-#$osname1= systeminfo | Select-String "OS Name:"
-#$osname2 = $osname1.ToString().Split(":")[1].Trim()
-
 $osname1 = $values.OsName
-
-#$memory1 = systeminfo | select-String "Total Physical Memory:"
-#$memory2 = $memory1.ToString().Split(":")[1].Trim()
- 
 $memory1 = $values.OsTotalVisibleMemorySize/(1024*1024)
-
 $serialnumber1 = $values.BiosSeralNumber
-
-#$serialnumber = Get-WMIObject win32_bios | Select SerialNumber
-#$serialnumber2 = $serialnumber.SerialNumber
-
-#$systemmodel1 = systeminfo | select-String "System Model"
-#$systemmodel2 = $systemmodel1.ToString().Split(":")[1].Trim()
-
 $systemmodel1 = $values.CsModel
-
-#$ipaddress = ipconfig | select-string "IPv4 Address" 
-#$ipaddress2 = $ipaddress.ToString().Split(":")[1].Trim()  
-
-
 $Microprocessor = $values.CsProcessors.Name
-
 $ipaddress = $net_values.IPv4Address.IPAddress
-
 $licenseKMS_or_OEM = get-wmiObject -query "select * from SoftwareLicensingService"
-
-
 $user = whoami 
 
-$wifi = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} 
-                          
+# make systeminfo new object where we will save all inventory info
 
 $systeminfo = New-Object PSobject
 
@@ -66,10 +39,20 @@ Add-Member -InputObject $systeminfo -MemberType NoteProperty -Name Microprocesso
 Add-Member -InputObject $systeminfo -MemberType NoteProperty -Name Serial_Number $serialnumber1
 Add-Member -InputObject $systeminfo -MemberType NoteProperty -Name Model $systemmodel1
 
-# add wifi SSID and passwords
+
+# get full wifi values with netsh CMD command in case that service is enabled
+
+$wifiservice = (Get-Service -Name WlanSvc).Status
+
+if ($wifiservice -ne "Stopped") { 
+
+                                $wifi = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} 
+                                }
+
+# add wifi SSID and passwords if is available
 
 
-if ($wifi.PROFILE_NAME -ne $null -or $wifi.PASSWORD -ne $null) { 
+if ($null -ne $wifi.PROFILE_NAME -or $null -ne $wifi.PASSWORD) { 
 
                                                                Add-Member -InputObject $systeminfo -MemberType NoteProperty -Name SSID $wifi.PROFILE_NAME
                                                                Add-Member -InputObject $systeminfo -MemberType NoteProperty -Name Password $wifi.PASSWORD
@@ -86,19 +69,33 @@ if ($licenseKMS_or_OEM.SubscriptionEdition -eq "UNKNOWN") {
 
                                                           }
 
-elseif ($licenseKMS_or_OEM.OA3XOriginalProductKey -ne $null) { 
+elseif ($null -ne $licenseKMS_or_OEM.OA3XOriginalProductKey) { 
 
                                                               Add-Member -InputObject $systeminfo NoteProperty -Name OEMLicense $licenseKMS_or_OEM.OA3XOriginalProductKey 
 
                                                               } 
 
-elseif ($licenseKMS_or_OEM.DiscoveredKeyManagementServiceMachineName -ne $null) { 
+elseif ($null -ne $licenseKMS_or_OEM.DiscoveredKeyManagementServiceMachineName) { 
 
                                                                 Add-Member -InputObject $systeminfo NoteProperty -Name KMS_Server_License $licenseKMS_or_OEM.DiscoveredKeyManagementServiceMachineName 
 
                                                               } 
 
 
+
+# get the S.M.A.R.T values from physical disks
+
+$physicaldisk = Get-PhysicalDisk
+[int]$counter = 0
+
+foreach ($pd in $physicaldisk) {
+
+                                $Disk_name = "Physical Disk " + [string]$counter
+                                Add-Member -InputObject $systeminfo -MemberType NoteProperty -Name $Disk_name $pd.FriendlyName                  
+                                $status = "S.M.A.R.T Status Physical Disk" +  [string]$counter
+                                Add-Member -InputObject $systeminfo -MemberType NoteProperty -Name $status $pd.HealthStatus
+
+                                }
                                                                
 
 
@@ -124,7 +121,7 @@ foreach ($disk2 in $disk.DeviceID) {
 foreach ($totalsize in $disk.Size) { 
 
                                    
-                                   $size_in_GB = "Total_Size_in_GB_disk" + [string]$cont 
+                                   $size_in_GB = "Total_Size_in_GB in Drive " + [string]$cont 
                                    [int64]$diskGB = [int64]$totalsize / 1073741824
                                    Add-Member -InputObject $systeminfo -MemberType NoteProperty -Name $size_in_GB -Value $diskGB
                                    [int]$cont++
@@ -138,7 +135,7 @@ foreach ($totalfree in $disk.FreeSpace) {
 
                                    
                                    
-                                   $Free_in_GB = "Total_Freespace_GB_disk" + [string]$cont
+                                   $Free_in_GB = "Total_Freespace_GB in Drive " + [string]$cont
                                    [int64]$freeGB = [int64]$totalfree /1073741824
                                    Add-Member -InputObject $systeminfo -MemberType NoteProperty -Name $Free_in_GB -Value $freeGB
                                    [int]$cont++
@@ -148,9 +145,10 @@ foreach ($totalfree in $disk.FreeSpace) {
 
 
 
-
 $systeminfo | Out-GridView -Title "ComputerInfo"
  
+
+
 
 
 
